0167d726339de9649bf6e1c79ac9b1b4343fba67
Eve <e_sotnik@bk.ru>
2021-02-26 20:54:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string s = Console.ReadLine();
+            Console.WriteLine("Ваше число в обратном виде");
+            if (s[0] != '-')
+                for (int i = s.Length - 1; i > -1; --i)
+                    Console.Write((s[i]));
+            else
+            {
+                Console.Write("-");
+                for (int i = s.Length - 1; i > 0; --i)
+                    Console.Write((s[i]));
+            }
+            Console.Read();
+        }
+    }
+}
